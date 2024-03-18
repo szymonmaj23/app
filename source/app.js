a383fdf4cf6d6cf89f99357b3a84cde1d9f9ec61
szymonmaj23 <szymimaja23@gmail.com>
2024-03-18 14:30:47 +0100

    strona informacyjna

--- source/app.js
+++ source/app.js
@@ -1,22 +1,43 @@
 var express = require('express');
-
 var app = express();
 
+// Obsługa strony głównej
 app.get('/', function(req, res){
-	res.send('DevOps - Strona startowa!');
-	console.log('Zażądano strony Start');
+    res.send('<a href="/users">Przejdź do strony użytkownika</a><br>' +
+             '<a href="/services">Przejdź do strony serwisu</a><br>'
+			 '<a href="/users">Przejdź do strony informacyjnej</a><br> +
+             'DevOps - Strona startowa!');
+    console.log('Zażądano strony Start');
 });
 
+// Obsługa strony użytkownika
 app.get('/users', function(req, res){
-	res.send('DevOps - strona Users!');
-	console.log('Zażądano strony Users');
+    res.send('<a href="/">Powrót do strony startowej</a><br>' +
+             '<a href="/services">Przejdź do strony serwisu</a><br>'
+			 '<a href="/users">Przejdź do strony informacyjnej</a><br> +
+             'DevOps - strona Users!');
+    console.log('Zażądano strony Users');
 });
 
+// Obsługa strony serwisu
 app.get('/services', function(req, res){
-	res.send('DevOps - strona Services!');
-	console.log('Zażądano strony Services');
+    res.send('<a href="/">Powrót do strony startowej</a><br>' +
+             '<a href="/users">Przejdź do strony użytkownika</a><br>'
+			 '<a href="/users">Przejdź do strony informacyjnej</a><br> +
+             'DevOps - strona Services!');
+    console.log('Zażądano strony Services');
 });
 
+// Obsługa strony Info
+app.get('/services', function(req, res){
+    res.send('<a href="/">Powrót do strony startowej</a><br>' +
+             '<a href="/users">Przejdź do strony użytkownika</a><br>'
+			 '<a href="/users">Przejdź do strony serwisu</a><br> +
+             'DevOps - strona Info!');
+    console.log('Zażądano strony Info');
+});
+
+
+// Start serwera
 app.listen(3001, function(){
-	console.log('Aplikacja OK');
-}); 
+"app.js" 
