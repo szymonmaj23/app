52337a40de2cb7d8038bc91b16289e6d1e407cd7
szymonmaj23 <szymimaja23@gmail.com>
2024-03-18 11:33:44 +0100

    strona services

---
+++ source/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('DevOps - Strona startowa!');
+	console.log('Zażądano strony Start');
+});
+
+app.get('/users', function(req, res){
+	res.send('DevOps - strona Users!');
+	console.log('Zażądano strony Users');
+});
+
+app.get('/services', function(req, res){
+	res.send('DevOps - strona Services!');
+	console.log('Zażądano strony Services');
+});
+
+app.listen(3001, function(){
+	console.log('Aplikacja OK');
+}); 
